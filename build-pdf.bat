d95c75f2fb39ddf4af94035741a8fab86bfa6670
Daniel Green <KasumiL5x@gmail.com>
2016-02-16 17:18:09 +0000

    Build bat improved a little.

--- build-pdf.bat
+++ build-pdf.bat
@@ -1,14 +1,14 @@
 @echo off
 
-:: create temp directory
-mkdir tmp
-
-:: create out directory
-mkdir out
-
 :: configure out and tmp directory
 set INT_DIR=tmp
 set OUT_DIR=out
+
+:: create temp directory
+if not exist %INT_DIR% mkdir %INT_DIR%
+
+:: create out directory
+if not exist %OUT_DIR% mkdir %OUT_DIR%
 
 :: run pdflatex
 pdflatex -shell-escape -output-directory=%OUT_DIR% -aux-directory=%INT_DIR% main
